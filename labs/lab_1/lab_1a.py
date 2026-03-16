623f89f8d3dfe3ac81db3400bf691175c0687b20
lain7209 <ianardenlee@gmail.com>
2026-03-15 21:14:47 -0700

    Update lab_1a.py
    added in a quick about me

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Ian Lee" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(" This is the first lab, and it is meant to be a simple introduction to Git and Python. In my free time, I like to bike around my neighborhood.")
 
 if __name__ == "__main__":
     main()
